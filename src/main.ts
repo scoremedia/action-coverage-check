b9782aa7a3d9998839ca7ad8fa16e64234796648
Akash Amin <akash.amin@thescore.com>
2025-10-01 12:24:49 -0400

    Clear message when changed files have coverage issues

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function run(): Promise<void> {
     const pullRequest = github.context.payload.pull_request
     const headSha = (pullRequest && pullRequest.head.sha) || github.context.sha
     const link = (pullRequest && pullRequest.html_url) || github.context.ref
-    const isSuccessful = totalCoverageInfo.totalCoverage >= 0.8 && totalCoverageInfo.annotations.length === 0
+    const isSuccessful = totalCoverageInfo.totalCoverage >= 1.0 && totalCoverageInfo.annotations.length === 0
     const totalCoverageStr = (totalCoverageInfo.totalCoverage * 100).toFixed(2)
     const conclusion: 'success' | 'failure' = isSuccessful
       ? 'success'
@@ -90,7 +90,7 @@ async function run(): Promise<void> {
       if (!isSuccessful) {
         const checkId = checkRequest.data.id
         const commentBody =
-          `:x: Uh-oh! Coverage dropped: ` +
+          `:x: Uh-oh! ${totalCoverageInfo.annotations.length > 0 ? `Coverage dropped on changed files: ${totalCoverageInfo.annotations.length} issues found.` : 'Overall project coverage dropped:'} ` +
           `https://github.com/${repoOwner}/${repoName}/runs/${String(checkId)}` +
           `\nOverall Project Coverage: ${totalCoverageStr}% ` +
           '\n' +
